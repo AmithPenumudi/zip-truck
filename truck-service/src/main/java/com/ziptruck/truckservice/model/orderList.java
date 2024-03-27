860058641a7ae2fd1eb888253260863382bcb9b5
SteelBuns <sasankpentela@gmail.com>
2024-03-27 13:18:37 +0530

    IPC Done

---
+++ truck-service/src/main/java/com/ziptruck/truckservice/model/orderList.java
@@ -0,0 +1,18 @@
+package com.ziptruck.truckservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_order_line_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class orderList {
+@Id
+    private Long orderId;
+}
